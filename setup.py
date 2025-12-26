5973674a4b3397f8c1e70690c8e7fbb43a5f650f
codewithdark-git <codewithdark90@gmail.com>
2025-12-26 11:06:09 +0500

    Add initial project files including .gitignore, LICENSE, pyproject.toml, requirements.txt, and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""Setup script for pip install compatibility."""
+
+from setuptools import setup
+
+# This file is needed for `pip install git+...` to work properly
+# All configuration is in pyproject.toml
+setup()
+
